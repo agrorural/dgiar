bfe6af5b306cd7b8341dc722fab3ea5fa54b51f0
Marcial Soto <marcialsotosanchez@gmail.com>
2018-06-04 15:28:51 -0500

    Todos los departamentos

--- resources/assets/js/map.js
+++ resources/assets/js/map.js
@@ -1,6 +1,7 @@
 let tipoMapa = 1;
 let map;
-let dgiarDepID = '01,02,03';
+
+let dgiarDepID = '01,02,03,04,05,06,08,09,10,11,12,13,14,15,18,20,21,22,23';
 let deno = '';
 let tipo = '';
 let depID = '';
